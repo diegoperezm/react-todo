ceef8c7b9aab9c7a68e3ff2e6c839c0c0755702a
erretres <diegoperezcorreo@gmail.com>
2020-10-23 12:39:32 -0300

    Refactor todoReducer.
    Replace if statements with an object.
    The object is easier to read.

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,88 +1,73 @@
-function todoReducer(state, event){
-
-   if(state.status === 'idle' && event.type === 'CREATE' ) {
-      return {
-          ...state,
-         status: 'creating',
-        }; 
-    }
-
-    if(state.status === 'creating' && event.type === 'FETCH')  {
-      return {
-        ...state,
-        status: 'loading'
-        }; 
-    }
-    
-    if(state.status === 'idle' && event.type === 'UPDATE' ) {
-      return {
-        
-          ...state, 
-          status: 'updating',
-          id: event.id
-        }; 
-    }
-
-    if(state.status === 'updating' && event.type === 'FETCH')  {
-      return {
-         ...state,
-        status: 'loading'
-        }; 
-    }
-
-    if(state.status === 'idle' && event.type === 'DELETE' ) {
-      return {
-          ...state,
-          status: 'deleting',
-          id: event.id
-        }; 
-    }
-
-    if(state.status === 'deleting' && event.type === 'FETCH')  {
-      return {
-        ...state,
-        status: 'loading'
-        }; 
-    }
-
-    if(state.status === 'deleting' && event.type === 'REJECT')  {
-      return {
-        ...state,
-          error: event.error,
-         status: 'failure'
-        }; 
-    }
-
-    if(state.status === 'idle' && event.type === 'FETCH' ) {
-      return {
-        ...state,
-        status: 'loading'
-        }; 
-    }
-
-    if(state.status === 'loading' && event.type === 'RESOLVE' ) {
-      return {
-          ...state,
-         data: event.data,
-         status: 'idle'
-      } ;
-    }
-
-    if(state.status === 'loading' && event.type === 'REJECT' ) {
-      return {
-          ...state,
-         error: event.error,  
-         status: 'failure',
-        }; 
-    }
-    if(state.status === 'failure' && event.type === 'FETCH' ) {
-      return {
-          ...state,
-         error: event.err,  
-         status: 'idle',
-        }; 
-    }
-    return state;
+function todoReducer(state, event) {
+  const NEXTSTATE = {
+      idle: {
+          FETCH:  {
+              ...state,
+              status:  'loading',
+          }, 
+          CREATE: { 
+              ...state,
+               status:  'creating'
+          },
+          UPDATE: {
+              ...state,
+              status:  'updating',
+              id: event.id
+          },
+          DELETE: {
+              ...state,
+              status:  'deleting',
+              id: event.id
+          }
+      },
+      creating: {
+          FETCH:  {
+              ...state,
+              status:  'loading'
+          }, 
+      },
+      updating: {
+          FETCH:  {
+              ...state,
+              status:  'loading',
+          }, 
+      },
+      deleting: {
+          FETCH:  {
+              ...state,
+              status:  'loading'  
+          }, 
+          REJECT:  {
+              ...state,
+              status: 'failure',
+              error: event.error
+          }
+      },
+      loading: {
+          RESOLVE: {
+              ...state,
+              status: 'idle',
+              data: event.data
+          },
+          REJECT:  {
+              ...state,
+              status: 'failure',
+              error: event.error
+          }
+      },
+      failure: {
+          FETCH:   {
+              ...state,
+              status: 'idle',
+  //         error: event.error
+          }
+      }
 };
 
+    const nextState = NEXTSTATE[state.status][event.type];
+    return nextState !== undefined ? nextState : state;
+}
+
+
 export default todoReducer;
+
