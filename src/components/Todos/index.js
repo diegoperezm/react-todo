487e7471eda7e615199eb5088959f830e6d98628
erretres <diegoperezcorreo@gmail.com>
2020-10-31 13:39:52 -0300

    Remove import bootstrap.min.css from Todos.
    Is not required.

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -2,8 +2,6 @@ import   React                                  from 'react';
 import { useState, useEffect, useReducer}       from 'react';
 import   todoReducer                            from '../../reducers/todoReducer.js';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Row                                      from 'react-bootstrap/Row';
 import Col                                      from 'react-bootstrap/Col';
 import Form                                     from 'react-bootstrap/Form';
