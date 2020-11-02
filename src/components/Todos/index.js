f46ad01ecb7c94526989bb3ccef1fbfd26d201d8
erretres <diegoperezcorreo@gmail.com>
2020-11-02 19:15:59 -0300

    Replace hardcoded string with env variable (axios url)

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -11,6 +11,9 @@ import Button                                   from 'react-bootstrap/Button';
 import List                                     from  '../List/';
 
 import axios                                    from 'axios';
+const  API                                      = process.env.REACT_APP_API; 
+
+
 
 function Todos() {
   const   initialState        = {
@@ -32,7 +35,7 @@ function Todos() {
        try {
            await axios
                      .post(
-                         'http://localhost:5000/todo',
+                         API, 
                          {
                              data: query,
                              isCompleted: false 
@@ -48,7 +51,7 @@ function Todos() {
        try {
            await axios
                      .post(
-                         'http://localhost:5000/todo',
+                          API, 
                          {
                              data: state.noEntQuery,
                              isCompleted: state.isCompleted
@@ -62,7 +65,7 @@ function Todos() {
 
     async  function read() {
        try {
-         const res  = await axios.get('http://localhost:5000/todo');
+         const res  = await axios.get(API);
          const data = await res.data.data;
          await dispatch({type: 'resolve',  data});
        } catch(error) {
@@ -72,7 +75,7 @@ function Todos() {
 
    async  function update() {
       try {
-        await axios.put('http://localhost:5000/todo', {id: state.id});
+        await axios.put(API, {id: state.id});
         await dispatch({type: 'fetch'});
       } catch(error) {
           if(error.message === 'Request failed with status code 409') {
@@ -90,7 +93,7 @@ function Todos() {
       try {
           await axios({
               method: 'DELETE',
-               url: 'http://localhost:5000/todo',  
+               url: API,  
                 headers: {
                   Accept: 'application/json',
                   'Content-Type': 'application/json'
